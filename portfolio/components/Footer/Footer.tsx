c4e064ed7b7bffa9a615962c7e70c5c311f75eb0
Klaudia Zygmunt <kzygmunt0@gmail.com>
2023-02-15 12:12:16 +0100

    correct: styles

--- portfolio/components/Footer/Footer.tsx
+++ portfolio/components/Footer/Footer.tsx
@@ -2,8 +2,8 @@ import styles from "./Footer.module.scss";
 
 const Footer: React.FC = () => {
   return (
-    <div className={styles.footer}>
-      <span className={styles.text}>Copyright © {new Date().getFullYear()} Klaudia Zygmunt</span>
+    <div className={styles.position}>
+      <span className={styles.text}>© {new Date().getFullYear()} Klaudia Zygmunt</span>
     </div>
   );
 };
