3cf16f24719d83c3ebd7f7787d8c91b06e5e77ea
Klaudia Zygmunt <kzygmunt0@gmail.com>
2023-02-21 17:18:10 +0100

    add: mobile hamburger react

--- portfolio/components/Header/Header.tsx
+++ portfolio/components/Header/Header.tsx
@@ -1,57 +1,129 @@
+import { useState, useEffect } from "react";
 import Link from "next/link";
-import { ImEmbed } from "react-icons/im";
 import styles from './Header.module.scss';
+import Hamburger from "hamburger-react";
+import { ImEmbed } from "react-icons/im";
 
 const Header: React.FC = () => {
+    const [isOpen, setOpen] = useState(false);
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth <= 992);
+            setOpen(false);
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const handleClick = () => {
+        setOpen(!isOpen);
+    };
+
     return (
-        <div className={styles.section}>
+        <header className={styles.section}>
             <div className={styles.position}>
                 <div className={styles.logo}>
                     <Link href="/">
                         <ImEmbed size="3rem" color="#5f50fd" />
                     </Link>
                 </div>
-                <nav className={styles.menu}>
-                    <ul className={styles.list}>
-                        <li>
-                            <Link href="/">
-                                <span className={styles.navlink}>
-                                    Home
-                                </span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/projects">
-                                <span className={styles.navlink}>
-                                    Projects
-                                </span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/skills">
-                                <span className={styles.navlink}>
-                                    Skills
-                                </span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/about">
-                                <span className={styles.navlink}>
-                                    About
-                                </span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/contact">
-                                <span className={styles.btn}>
-                                    Contact
-                                </span>
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
+                {isSmallScreen ? (
+                    <Hamburger
+                        toggled={isOpen}
+                        toggle={handleClick}
+                        label="Show menu"
+                    />
+                ) : (
+                    <nav className={styles.menu}>
+                        <ul className={styles.list}>
+                            <li>
+                                <Link href="/">
+                                    <span className={styles.navlink}>
+                                        Home
+                                    </span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/projects">
+                                    <span className={styles.navlink}>
+                                        Projects
+                                    </span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/skills">
+                                    <span className={styles.navlink}>
+                                        Skills
+                                    </span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/about">
+                                    <span className={styles.navlink}>
+                                        About
+                                    </span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/contact">
+                                    <span className={styles.btn}>
+                                        Contact
+                                    </span>
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                )}
+                {isOpen && isSmallScreen && (
+                    <nav className={`${styles.menu} ${isOpen ? styles.open : ""}`}>
+                        <ul className={styles.list}>
+                            <li>
+                                <Link href="/">
+                                    <span className={styles.navlink}>
+                                        Home
+                                    </span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/projects">
+                                    <span className={styles.navlink}>
+                                        Projects
+                                    </span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/skills">
+                                    <span className={styles.navlink}>
+                                        Skills
+                                    </span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/about">
+                                    <span className={styles.navlink}>
+                                        About
+                                    </span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/contact">
+                                    <span className={styles.btn}>
+                                        Contact
+                                    </span>
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                )}
             </div>
-        </div>
+        </header>
     );
 }
 
